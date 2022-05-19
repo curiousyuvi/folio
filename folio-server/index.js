0760e5707a59092d0a0ee65fa1af8bddfb3efa94
Yuvraj Singh <ysgaur9919@gmail.com>
2022-05-19 19:22:49 +0530

    create: created folio mern app

---
+++ folio-server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+require("./db/conn");
+const User = require("./models/userSchema");
+app.use(cors());
+app.use(express.json());
+app.use(require("./router/user"));
+
+app.listen("5000", () => {
+  console.log("server listening on port 5000...");
+});
